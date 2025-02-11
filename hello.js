d6526b1daeada5430a44102eb2548db62aba4150
GumasteShubham97 <shubhamgumaste97@gmail.com>
2025-02-11 09:26:51 +0530

    node middleware

--- hello.js
+++ hello.js
@@ -1,11 +1,30 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const routes = require('./routes');
+//console.log(routes.someText);
 
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
-server.listen(3000);
+// app.use((req, res, next) => {
+//     console.log('In the middleware!');
+//     next(); // Allows the request to continue to the next middleware in line
+// });
 
+app.use('/',(req, res, next) => {
+    console.log('This always runs!');
+    next();
+});
+
+app.use('/add-product',(req, res, next) => {
+    console.log('In the another middleware!');
+    res.send('<h1>Add Product Page</h1>');
+});
+
+app.use('/',(req, res, next) => {
+    console.log('In the another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+});
+// const server = http.createServer(app);
+// server.listen(3000);
+app.listen(3000);
 
 // Path: hello.test.js
 // for these kindd of
